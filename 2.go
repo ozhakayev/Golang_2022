af6d16816ca8c1db77117e06d0e32449de8b9833
ozhakayev <zhakayev.zh@gmail.com>
2022-04-03 20:28:49 +0600

    https://github.com/ozhakayev/Golang_2022

--- 2.go
+++ 2.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var count int = 20
+	var count int = 22
 	unitWeight := 0.4
 	totalWeight := float64(count) * unitWeight
 	fmt.Println(count, "cans weigh", totalWeight, "kilogram")
